9e7a1c34066fd3f8c4ee7e555e79045667be1152
euviniciuss <dev.mvsc@gmail.com>
2022-07-24 11:14:47 -0300

    feat: adicionando cadastro de conta

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,36 @@
-import express from 'express'
+import express, { json } from 'express'
+import { v4 as uuid } from 'uuid'
 
 const app = express()
 
-app.get('/courses', (request, response) => {
-  return response.json({ message: "Hello World!" })
+app.use(express.json())
+
+const customers: Customers[] = []
+
+type Customers = {
+  cpf: string
+  name: string
+  id: string
+  statement: Array<string>
+}
+
+app.post("/account", (request, response) => {
+  const id = uuid()
+
+  const { cpf, name } = request.body
+
+  const data: Customers = {
+    id,
+    cpf,
+    name,
+    statement: []
+  }
+
+  customers.push(data)
+
+  console.log(customers);
+  
+  return response.status(201).json({ message: "Conta criada com sucesso!" })
 })
 
 app.listen(3333)
